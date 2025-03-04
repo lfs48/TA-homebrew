e7774ed7d0b9b86a9da54aec08b6f6f9d5e6cfcc
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-03-04 15:10:49 -0500

    feat: add sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
+    import { ArrowRightSLineArrows } from 'svelte-remix';
     import { RouterView } from '@dvcol/svelte-simple-router/components';
 
-    import AscentPage from './routes/anomaly/ascent/+page.svelte';
-    import CoachPage from './routes/competency/coach/+page.svelte';
-    import ObsessedPage from './routes/reality/obsessed/+page.svelte'
+    import AscentPage from './routes/anomaly/ascent.svelte';
+
+    import CoachPage from './routes/competency/coach.svelte';
+    import SecretaryPage from './routes/competency/analyst/+page.svelte';
+
+    import ObsessedPage from './routes/reality/obsessed.svelte'
 
     const routes = [
         {
@@ -13,6 +17,10 @@
         {
             path: '/competency/coach',
             component: CoachPage
+        },
+        {
+            path: 'competency/secretary',
+            component: SecretaryPage
         },
         {
             path: '/reality/obsessed',
@@ -25,4 +33,37 @@
     };
 </script>
 
-<RouterView {options} />
+<div class="w-screen h-screen">
+    <div class="flex w-full h-full bg-zinc-800 font-terminal text-zinc-200">
+        <aside class={`
+            flex flex-col w-[9rem] h-full pl-2 py-8 space-y-3
+            border-r-2 border-r-zinc-200 border-opacity-20
+            text-[1rem] leading-none
+        `}>
+            <div class="flex items-center space-x-1">
+                <ArrowRightSLineArrows class="size-[1rem]"/>
+                <span>home</span>
+            </div>
+            <div class="flex items-center space-x-1 text-anomaly-blue">
+                <ArrowRightSLineArrows class="size-[1rem]"/>
+                <span>anomaly</span>
+            </div>
+            <div class="flex items-center space-x-1 text-reality-yellow">
+                <ArrowRightSLineArrows class="size-[1rem]"/>
+                <span>reality</span>
+            </div>
+            <div class="flex items-center space-x-1 text-agency-red">
+                <ArrowRightSLineArrows class="size-[1rem]"/>
+                <span>competency</span>
+            </div>
+            <div class="flex items-center space-x-1">
+                <ArrowRightSLineArrows class="size-[1rem]"/>
+                <span>playwall</span>
+            </div>
+        </aside>
+        <main class="w-full">
+            <header class="font-bold text-[2rem] p-8 leading-none">Placeholder Name</header>
+            <RouterView {options} />
+        </main>
+    </div>
+</div>
