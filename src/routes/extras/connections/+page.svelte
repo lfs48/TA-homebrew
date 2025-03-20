e0ecdaf42e412ccef81460fdfb00d1b1711b2574
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-03-20 17:56:23 -0400

    feat: add extras/connections and extras/requisitions pages

---
+++ src/routes/extras/connections/+page.svelte
@@ -0,0 +1,135 @@
+<style>
+    @reference "../../../app.css";
+    h1, h2 {
+        @apply
+        font-bold
+        text-reality-yellow
+        leading-none
+    ;}
+    h2 {
+        @apply
+        w-full 
+        border-b 
+        pb-1 
+        mb-1 
+        border-reality-yellow
+        text-[2rem]
+    ;}
+    p {
+        @apply
+        leading-[1.25rem]
+    ;}
+    li {
+        @apply
+        leading-none
+    ;}
+</style>
+
+<div class="w-full h-full py-8 pl-8 pr-24 space-y-8">
+    <div class="space-y-2">
+        <h1 class="text-[3rem]">Connection Bonuses</h1>
+        <p>
+            At your GM's discretion, the following <yellow>Connection Bonuses</yellow>
+            may be made available to assign to Relationships at your branch.
+        </p>
+    </div>
+    <div>
+        <h2>Advocate</h2>
+        <div>
+            <p>
+                This person is great at finding loopholes
+                to get people out of trouble.
+                Up to <red>3</red> times during your mission
+                when you would receive <red>one Demerit</red>,
+                you can explain the context to them
+                and ask them to argue in your defense.
+                They will provide your GM a compelling reason why
+                the <red>Demerit</red> wasn't warranted,
+                and you won't receive it. Revealing too much
+                information can still create a <b>Loose End</b>.
+            </p>
+        </div>
+    </div>
+    <div>
+        <h2>Benefactor</h2>
+        <div>
+            <p>
+                This person has tons of money and likes to spend it on you.
+                They give you a high-end credit card you can use on your next mission.
+                It can cover any small purchases you make, and one
+                obscenely expensive one. People who see you with this card
+                will assume you are a big spender and treat you accordingly.
+            </p>
+        </div>
+    </div>
+    <div>
+        <h2>Epicurean</h2>
+        <div>
+            <p>
+                This person is an excellent cook.
+                They will prepare homemade snacks
+                for you to take on your mission,
+                enough for <red>3</red> servings.
+                Anyone who eats a serving of these snacks
+                ignores all <b>Burnout</b> on their next roll.
+            </p>
+        </div>
+    </div>
+    <div>
+        <h2>Forecaster</h2>
+        <div>
+            <p>
+                This person hosts a program where they make
+                predictions about a specific topic:
+                weather, finance, social media trends, etc.
+                You can tune in once during your mission
+                to catch them giving today's forecast.
+                Whatever predictions they make related
+                to their subject of expertise will come to pass.
+            </p>
+        </div>
+    </div>
+    <div>
+        <h2>Gaslighter</h2>
+        <div>
+            <p>
+                This person loves to lie for fun.
+                Once during your mission you can ask them
+                to help you convince a group of people
+                of something that's blatantly untrue,
+                and the targets will believe it. If you do anything later
+                to expose or contradict the lie, you lose
+                <yellow>one Connection</yellow> with this Relationship.
+            </p>
+        </div>
+    </div>
+    <div>
+        <h2>Paranoid</h2>
+        <div>
+            <p>
+                This person lives in constant fear.
+                Once during your mission, you can suggest someone
+                or something is out to get them. They will
+                keep an eye on it from afar for the rest
+                of the mission, communicating any major
+                developments to you. Should they witness anything
+                <blue>Anomalous</blue>, their paranoid nature
+                will keep them from spreading that knowledge:
+                their <b>Loose End</b> count will not grow
+                beyond one while this <b>Bonus</b> is active.
+            </p>
+        </div>
+    </div>
+    <div>
+        <h2>Repulsive</h2>
+        <div>
+            <p>
+                No one wants to be near this person.
+                Once during your mission, you can ask them
+                to approach a mundane person or group of people.
+                They target(s) will quickly find an excuse to leave the area
+                and won't return until your Relationship is gone.
+            </p>
+        </div>
+    </div>
+</div>
---
+++ src/routes/extras/requisitions/+page.svelte
@@ -0,0 +1,125 @@
+<script>
+    import { ArrowRightSLineArrows } from 'svelte-remix';
+</script>
+
+<style>
+    @reference "../../../app.css";
+    h1, h2 {
+        @apply
+        font-bold
+        text-agency-red
+        leading-none
+    ;}
+    h2 {
+        @apply
+        w-full 
+        border-b 
+        mb-1 
+        border-agency-red
+        text-[2rem]
+    ;}
+    p {
+        @apply
+        leading-[1.25rem]
+    ;}
+    li {
+        @apply
+        leading-none
+    ;}
+</style>
+
+{#snippet listItem(text)}
+    <div class="flex items-center space-x-2">
+        <ArrowRightSLineArrows class="text-agency-red size-[1rem]"/>
+        <div class="leading-none">{@html text}</div>
+    </div>
+{/snippet}
+
+<div class="w-full h-full py-8 pl-8 pr-24 space-y-8">
+    <div class="space-y-2">
+        <h1 class="text-[3rem]">Requisitions</h1>
+        <p>
+            At your GM's discretion, the following <red>Requisitions</red> may be made
+            available for purchase at your branch.
+        </p>
+    </div>
+    <div>
+        <h2>Positive Affirmations</h2>
+        <div class="space-y-2">
+            <p>
+                You receive a miniature MP<red>3</red> player,
+                containing recordings of short uplifting phrases
+                being spoken by the voice of a mundane person of your choice
+                in a warm and reassuring tone.
+            </p>
+            <p>
+                Each time the play button is pressed,
+                one of the following phrases is played at random:
+            </p>
+            <div class="grid grid-cols-3 gap-1">
+                {#each [
+                    "You are good enough.",
+                    "Believe in yourself.",
+                    "You are performing adequately.",
+                    "Reality will be stabilized.",
+                    "I love you.",
+                    "You can do anything.",
+                    "Don't give up!",
+                    "You will successfully capture this <blue>Anomaly</blue>.",
+                    "Everything will be okay.",
+                ] as phrase}
+                    {@render listItem(phrase)}
+                {/each}
+            </div>
+            <div class="flex justify-between">
+                <div class="space-y-0.5">
+                    <p><b>Basic Purchase:</b> <red>6 Commendations</red></p>
+                    <p><b>Custom Phrase Add-On:</b> <red>3 Commendations per Word</red></p>
+                </div>
+                <p class="text-end text-[0.5rem]">
+                    <b>Disclaimer:</b> Recorded statements may not accurately reflect Reality.
+                    Recorded statements do not represent the views
+                    of the <red>Triangle Agency</red> or its employees.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="space-y-2">
+        <h2>Unreal Estate</h2>
+        <p>
+            The <red>Agency</red> owns and manages a number of residential properties,
+            including an apartment whose location is central and convenient to the mission
+            you're currently on. You can rent this apartment for the duration of your mission.
+        </p>
+        <p>
+            Apartments include basic furnishings and utilities.
+            The penthouse apartment also features luxury amenities
+            including an indoor pool, private theater,
+            and helipad (helicopter not included).
+        </p>
+        <p>
+            Mundane people cannot locate this apartment without directions from you
+            and cannot enter it uninvited.
+        </p>
+        <div class="space-y-0.5">
+            <p><b><red>3</red>-Bedroom Rental:</b> <red>3 Commendations</red></p>
+            <p><b>Penthouse Rental:</b> <red>9 Commendations</red></p>
+        </div>
+    </div>
+    <div>
+        <h2>UV300 Triangular Shades</h2>
+        <div class="space-y-2">
+            <p>
+                In addition to making you look incredibly stylish
+                and protection your eyes from hurtful radiation,
+                these red-tinted <red>triangle</red>-lensed sunglasses will
+                highlight nearby people and objects in your field of view
+                that may be helpful to your investigation.
+            </p>
+            <div class="space-y-0.5">
+                <p><b>Standard Pair:</b> <red>15 Commendations</red></p>
+                <p><b>Anti-Hypnosis Coating:</b> <red>15 Additional Commendations</red></p>
+            </div>
+        </div>
+    </div>
+</div>
