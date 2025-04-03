b4362cb42a9e8d0ff823f6a42d33d6fdf3bd55f0
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-04-03 19:42:52 -0400

    fix: set adapter fallback key

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(process.env.NODE_ENV === 'production' ? 'app.html' : 'index.html'),
+		adapter: adapter({
+			fallback: process.env.NODE_ENV === 'production' ? 'app.html' : 'index.html'
+		}),
 	},
 	preprocess: [vitePreprocess()]
 };
