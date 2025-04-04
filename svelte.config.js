ad36b530d003a043414560a33b80b6d1f37e3fe5
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-04-03 21:50:22 -0400

    fix: try index.html for fallback

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: process.env.NODE_ENV === 'production' ? 'app.html' : 'index.html'
+			fallback: 'index.html'
 		}),
 	},
 	preprocess: [vitePreprocess()]
