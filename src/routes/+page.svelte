989184b16f31409cc975ed22b196cb930a6e577b
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-04-04 16:03:40 -0400

    tweak: update landing page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,42 @@
+<script>
+    const txt = [
+        'Congratulations, General Manager!',
+        `
+            Your branch has been chosen to participate in <penta>Project Pentachoron</penta>,
+            an exciting new initiative that will extend the <red>Agency's</red> reach into
+            new dimensions.
+        `,
+        `
+            To opt your field team into <penta>Project Pentachoron</penta>, simply finish reading this sentence.
+        `
+    ];
+    const i = $state(0);
+    const j = $state(0);
+
+    function type() {
+        
+    }
+</script>
+
 <div class="w-full h-full py-8 pl-8 pr-24 space-y-8">
-    <p>Congratulations, <red>Agent!</red></p>
-    <div>
-        Your branch has been selected to participate
-        in the internal alpha test for <penta>Project Pentachoron</penta>.
-        This exciting new <red>Agency</red> initiative seeks to
-        modernize and streamline the distribution of <red>Agent</red>-facing materials
-        through a sleek digital interface.
+    <div class="space-y-2 pb-4">
+        <p class="pb-2">Congratulations, General Manager!</p>
+        <p>
+            By accessing this hypertext document, you have opted into participating 
+            in <penta>Project Pentachoron</penta>. This exciting new initiative 
+            will promote connection and synergy between the five
+            <red>Agency</red>-favorable timelines by offering materials from
+            alternate chronologies to your branch.
+        </p>
+        <p>
+            At your discretion, you may direct <red>Agents</red> on your Field Team
+            to visit this page and consider the materials presented therein as
+            a supplement to their Field Agent Manuals.
+        </p>
+        <p>
+            If you are an <red>Agent</red> from a non-participating branch who has
+            erroneously accessed this document, please coax your General Manager
+            into visiting this page so that your access is retroactively justified.
+        </p>
     </div>
 </div>
