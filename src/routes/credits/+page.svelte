025ab3d1376860b712f1bf7ad11e965d43911366
Lucas Schraier <lucas.schraier@shipsigma.com>
2025-09-23 16:30:00 -0400

    Update credits with playtester

--- src/routes/credits/+page.svelte
+++ src/routes/credits/+page.svelte
@@ -1,6 +1,8 @@
 <div class="py-8 pl-8 pr-24 leading-none">
     <p class="text-[1.25rem] pb-3"><b>Lead Designer:</b> L. Schraier</p>
-    <p class="text-[1.25rem] pb-8"><b>Additional Design & Editing:</b> Robert Joseph</p>
+    <p class="text-[1.25rem] pb-3"><b>Additional Design & Editing:</b> Robert Joseph</p>
+    <p class="text-[1rem] pb-8"><b>Playtesting:</b> Ash, Bach, Doomslug, Nikki, Onion, Wyllora</p>
+
     <p class="pb-3"><penta>Project Pentachoron</penta> is an independent production by L. Schraier and others,
         and is not affiliated with <red>Triangle Agency</red> or <red>Haunted Table, LLC.</red>
         It is published under the <red>Triangle Agency Third Party License.</red>
